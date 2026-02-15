bf0e9e1d07c3182bcf5e8267d6496ae97c353cf1
Marco gortari <87701959+Mgortari@users.noreply.github.com>
2026-02-15 18:37:19 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
